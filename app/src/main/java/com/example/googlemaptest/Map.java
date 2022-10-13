d1a5793264d433661ad2b2ef02f8137f25f36335
jayjang0209 <77429796+jayjang0209@users.noreply.github.com>
2022-10-12 23:51:08 -0700

    Fix code warnings

--- app/src/main/java/com/example/googlemaptest/Map.java
+++ app/src/main/java/com/example/googlemaptest/Map.java
@@ -1,7 +1,5 @@
 package com.example.googlemaptest;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
@@ -10,12 +8,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Objects;
 
 public class Map extends Fragment {
 
@@ -33,37 +31,18 @@ public class Map extends Fragment {
                 .findFragmentById(R.id.map);
 
         // Async map
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull GoogleMap googleMap) {
-                googleMap = googleMap;
+        Objects.requireNonNull(mapFragment).getMapAsync(googleMap -> {
 
-                // Add a marker in Sydney and move the camera
-                LatLng bcit = new LatLng(49.28357705407709, -123.11451451191841);
-                googleMap.addMarker(new MarkerOptions()
-                                .position(bcit)
-                                .title("We are here!!"))
-                        .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.cat_icon));
-                googleMap.moveCamera(CameraUpdateFactory.newLatLng(bcit));
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bcit, 12));
-            }
+            // Add a marker in Sydney and move the camera
+            LatLng bcit = new LatLng(49.28357705407709, -123.11451451191841);
+            Objects.requireNonNull(googleMap.addMarker(new MarkerOptions()
+                            .position(bcit)
+                            .title("We are here!!")))
+                    .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.cat_icon));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(bcit));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bcit, 12));
         });
 
         return view;
     }
-
-
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        googleMap = googleMap;
-//
-//        // Add a marker in Sydney and move the camera
-//        LatLng bcit = new LatLng(49.28357705407709, -123.11451451191841);
-//        googleMap.addMarker(new MarkerOptions()
-//                .position(bcit)
-//                .title("We are here!!"))
-//                .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.cat_icon));
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(bcit));
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bcit, 12));
-//    }
 }
