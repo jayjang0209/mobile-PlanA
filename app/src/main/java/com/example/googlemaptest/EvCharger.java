8ff8b26d258bd152380ba36cafaef52e4f1b8ad8
jayjang0209 <77429796+jayjang0209@users.noreply.github.com>
2022-10-12 23:31:42 -0700

    Add EvCharger activity

---
+++ app/src/main/java/com/example/googlemaptest/EvCharger.java
@@ -0,0 +1,47 @@
+package com.example.googlemaptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EvCharger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ev_charger);
+
+        // Initialize fragments
+        Fragment first = new Map();
+        Fragment second = new EvChargerConnectorList();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.ctnFragment, first);
+        fragmentTransaction.commit();
+
+
+        Button btnFirst = findViewById(R.id.buttonFragmentMap);
+        // only one method -> lambda
+
+        btnFirst.setOnClickListener(view -> {
+            FragmentTransaction fragmentTransaction1 = fragmentManager.beginTransaction();
+            fragmentTransaction1.replace(R.id.ctnFragment, first);
+            fragmentTransaction1.commit();
+        });
+
+        Button btnSecond = findViewById(R.id.buttonFragmentList);
+        btnSecond.setOnClickListener(view -> {
+            FragmentTransaction fragmentTransaction2 = fragmentManager.beginTransaction();
+            fragmentTransaction2.replace(R.id.ctnFragment, second);
+            fragmentTransaction2.commit();
+        });
+
+    }
+}
