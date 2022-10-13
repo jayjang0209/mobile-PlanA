febfcdd62d8f5ac2d510005321e4d596f9530071
jayjang0209 <77429796+jayjang0209@users.noreply.github.com>
2022-10-12 23:28:29 -0700

    Change Map to fragment

--- app/src/main/java/com/example/googlemaptest/Map.java
+++ app/src/main/java/com/example/googlemaptest/Map.java
@@ -1,8 +1,13 @@
 package com.example.googlemaptest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,29 +17,53 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends AppCompatActivity implements OnMapReadyCallback {
+public class Map extends Fragment {
+
+    public Map() {
+        super(R.layout.activity_map);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_map, container, false);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        // Initialize map fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        // Async map
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                googleMap = googleMap;
+
+                // Add a marker in Sydney and move the camera
+                LatLng bcit = new LatLng(49.28357705407709, -123.11451451191841);
+                googleMap.addMarker(new MarkerOptions()
+                                .position(bcit)
+                                .title("We are here!!"))
+                        .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.cat_icon));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(bcit));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bcit, 12));
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng bcit = new LatLng(49.28357705407709, -123.11451451191841);
-        googleMap.addMarker(new MarkerOptions()
-                .position(bcit)
-                .title("We are here!!"))
-                .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.cat_icon));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(bcit));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bcit, 12));
-    }
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        googleMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng bcit = new LatLng(49.28357705407709, -123.11451451191841);
+//        googleMap.addMarker(new MarkerOptions()
+//                .position(bcit)
+//                .title("We are here!!"))
+//                .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.cat_icon));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(bcit));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bcit, 12));
+//    }
 }
