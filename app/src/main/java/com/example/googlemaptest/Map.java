1b0110c6b3e6abce076e428ae628e760032001a1
jayjang0209 <77429796+jayjang0209@users.noreply.github.com>
2022-10-30 17:25:08 -0700

    Enable adding multiple markers from DB

--- app/src/main/java/com/example/googlemaptest/Map.java
+++ app/src/main/java/com/example/googlemaptest/Map.java
@@ -1,21 +1,34 @@
 package com.example.googlemaptest;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 import java.util.Objects;
 
 public class Map extends Fragment {
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     public Map() {
         super(R.layout.activity_map);
@@ -26,6 +39,11 @@ public class Map extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_map, container, false);
 
+        // get the instance of the Firebase database
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        // get the reference to the JSON tree
+        databaseReference = firebaseDatabase.getReference();
+
         // Initialize map fragment
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
@@ -33,12 +51,44 @@ public class Map extends Fragment {
         // Async map
         Objects.requireNonNull(mapFragment).getMapAsync(googleMap -> {
 
-            // Add a marker in Sydney and move the camera
+            databaseReference = firebaseDatabase.getReference();
+
+            databaseReference.child("EvCharingStations").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    int numStations = 0;
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        numStations += 1;
+                        double lat = Double.parseDouble(String.valueOf(snapshot.child("Latitude").getValue()));
+                        double lon = Double.parseDouble(String.valueOf(snapshot.child("Longitude").getValue()));
+                        String address = String.valueOf(snapshot.child("Street Address").getValue());
+                        LatLng station = new LatLng(lat, lon);
+
+                        // Set the size of marker in map
+                        int height = 100;
+                        int width = 100;
+                        BitmapDrawable bitmapdraw= (BitmapDrawable) getResources().getDrawable( R.drawable.ev_charing_station_icon);
+                        Bitmap b = bitmapdraw.getBitmap();
+                        final Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+
+                        // Add markers to the googleMap
+                        Objects.requireNonNull(googleMap.addMarker(new MarkerOptions()
+                                        .position(station)
+                                        .title(address)))
+                                .setIcon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+//                        Log.i("lat", String.valueOf(lat));
+                    }
+                    Log.i("Stations", String.valueOf(numStations));
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+
             LatLng bcit = new LatLng(49.28357705407709, -123.11451451191841);
-            Objects.requireNonNull(googleMap.addMarker(new MarkerOptions()
-                            .position(bcit)
-                            .title("We are here!!")))
-                    .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.cat_icon));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(bcit));
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bcit, 12));
         });
