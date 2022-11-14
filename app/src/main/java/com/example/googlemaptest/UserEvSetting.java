ea64009b8a09b72a0851ba0027a0d05e7704724b
jayjang0209 <77429796+jayjang0209@users.noreply.github.com>
2022-11-13 21:43:37 -0800

    Fix button id

--- app/src/main/java/com/example/googlemaptest/UserEvSetting.java
+++ app/src/main/java/com/example/googlemaptest/UserEvSetting.java
@@ -68,7 +68,7 @@ public class UserEvSetting extends AppCompatActivity {
         runner.execute(endPoint);
 
 
-        Button authenticate = findViewById(R.id.btnEvTripPlanner);
+        Button authenticate = findViewById(R.id.btnCreateEVDriverAccount);
         authenticate.setOnClickListener(view -> {
             Intent intent = new Intent(this, EvTripPlanner.class);
             startActivity(intent);
