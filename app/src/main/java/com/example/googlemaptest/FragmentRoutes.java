46e516efa63b44ebdd4f4568ee8b3b3b53ebfc82
msubin <joy120594@gmail.com>
2022-11-24 00:09:07 -0800

    Create savedRoute related files

---
+++ app/src/main/java/com/example/googlemaptest/FragmentRoutes.java
@@ -0,0 +1,14 @@
+package com.example.googlemaptest;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.ListFragment;
+
+public class FragmentRoutes extends ListFragment {
+    String[] routes;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
